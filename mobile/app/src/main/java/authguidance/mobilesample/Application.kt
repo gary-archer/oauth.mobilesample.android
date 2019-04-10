62df5a1811fdbe6068c056ad5cf58bb48ee60c99
Gary Archer <Gary.Archer@collinsongroup.com>
2019-04-10 08:58:17 +0100

    Updates

--- mobile/app/src/main/java/authguidance/mobilesample/Application.kt
+++ mobile/app/src/main/java/authguidance/mobilesample/Application.kt
@@ -1,7 +1,52 @@
 package authguidance.mobilesample
+import authguidance.mobilesample.plumbing.utilities.MobileLogger
 
 /*
  * Our custom application class
  */
 class Application : android.app.Application() {
+
+    private lateinit var defaultExceptionHandler: Thread.UncaughtExceptionHandler
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Override the default exception handler
+        this.defaultExceptionHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { t, e -> this.onException(t, e) }
+    }
+
+    /*
+     * Unhandled exceptions are caught and handled here
+     */
+    private fun onException(thread: Thread, exception: Throwable) {
+
+        // Do our handling
+        val logger = MobileLogger()
+        logger.debug("Unhandled exception")
+        val text = exception.message ?: exception.toString()
+        logger.debug(text)
+
+        // Call the default handler and allow the app to terminate
+        this.defaultExceptionHandler.uncaughtException(thread, exception)
+
+        // This looks promising
+        // https://github.com/Idolon-V/android-crash-catcher
+
+        /*
+        // HTTP errors
+        // https://medium.com/yammer-engineering/handling-generic-http-errors-across-your-android-app-eaff4e49bbb2
+
+        // Not working
+        // https://stackoverflow.com/questions/30255905/is-it-impossible-to-start-a-activity-in-uncaughtexceptionhandler
+        // https://stackoverflow.com/questions/6547072/how-do-i-start-another-activity-from-uncaughtexceptionhandler-uncaughtexception/10547176
+        val intent = Intent(this, ErrorActivity::class.java)
+        intent.putExtra("EXTRA_MY_EXCEPTION_HANDLER", BaseActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+
+        // intent.putExtra("Exception", exception)
+        this.startActivity(intent)
+        */
+    }
 }
--- mobile/app/src/main/java/authguidance/mobilesample/activities/BaseActivity.kt
+++ mobile/app/src/main/java/authguidance/mobilesample/activities/BaseActivity.kt
@@ -1,50 +1,9 @@
 package authguidance.mobilesample.activities
 
-import android.app.PendingIntent
-import android.content.Intent
-import android.os.Bundle
-import android.os.Looper
 import android.support.v7.app.AppCompatActivity
-import authguidance.mobilesample.plumbing.utilities.MobileLogger
 
 /*
- * A base activity to handle exceptions
+ * A base activity for our displays
  */
 abstract class BaseActivity : AppCompatActivity() {
-
-    /*
-     * Override and register an exception handler
-     */
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Thread.setDefaultUncaughtExceptionHandler { _, e -> this.onException(e) }
-    }
-
-    /*
-     * Unhandled exceptions are caught and we then transfer to the error activity
-     */
-    private fun onException(exception: Throwable) {
-
-        val logger = MobileLogger()
-        logger.debug("Unhandled exception")
-
-        val text = exception.message ?: exception.toString()
-        logger.debug(text)
-
-        // HTTP errors
-        // https://medium.com/yammer-engineering/handling-generic-http-errors-across-your-android-app-eaff4e49bbb2
-
-        // Not working
-        // https://stackoverflow.com/questions/30255905/is-it-impossible-to-start-a-activity-in-uncaughtexceptionhandler
-        // https://stackoverflow.com/questions/6547072/how-do-i-start-another-activity-from-uncaughtexceptionhandler-uncaughtexception/10547176
-        val intent = Intent(this, ErrorActivity::class.java)
-        intent.putExtra("EXTRA_MY_EXCEPTION_HANDLER", BaseActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-
-        // intent.putExtra("Exception", exception)
-        this.startActivity(intent)
-
-        logger.debug("Moved to error activity")
-    }
 }
