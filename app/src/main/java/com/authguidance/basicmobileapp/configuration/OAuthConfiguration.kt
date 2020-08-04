c43e85e8e9322e63e9d1ae8f97ce5a217ee74013
gary <gary@MACSTATION.local>
2020-08-04 14:36:12 +0100

    Updates

--- app/src/main/java/com/authguidance/basicmobileapp/configuration/OAuthConfiguration.kt
+++ app/src/main/java/com/authguidance/basicmobileapp/configuration/OAuthConfiguration.kt
@@ -11,23 +11,14 @@ class OAuthConfiguration {
     // The identifier for our mobile app
     lateinit var clientId: String
 
-    // The web domain name that hosts the interstitial page
-    lateinit var webDomain: String
+    // The mobile HTTPS domain name
+    lateinit var mobileBaseUrl: String
 
     // The interstitial page that receives the login response
     lateinit var loginRedirectPath: String
 
     // The interstitial page that receives the logout response
     lateinit var postLogoutRedirectPath: String
-
-    // The deep link domain, also used to receive claimed HTTPS scheme OAuth responses
-    lateinit var deepLinkDomain: String
-
-    // The deep linking path on which the app is invoked, after login
-    lateinit var loginActivatePath: String
-
-    // The deep linking path on which the app is invoked, after login
-    lateinit var postLogoutActivatePath: String
 
     // The Authorization Server endpoint used for logouts
     lateinit var logoutEndpoint: String
