7bae67c52046a8c59f008e8e4efcf25a72f8d62c
gary <garyarcher36@gmail.com>
2023-08-09 18:54:15 +0100

    Updates

--- app/src/main/java/com/authsamples/basicmobileapp/plumbing/oauth/AuthenticatorImpl.kt
+++ app/src/main/java/com/authsamples/basicmobileapp/plumbing/oauth/AuthenticatorImpl.kt
@@ -11,7 +11,6 @@ import com.authsamples.basicmobileapp.plumbing.oauth.logout.CognitoLogoutUrlBuil
 import com.authsamples.basicmobileapp.plumbing.oauth.logout.LogoutUrlBuilder
 import com.authsamples.basicmobileapp.plumbing.oauth.logout.StandardLogoutUrlBuilder
 import com.authsamples.basicmobileapp.plumbing.utilities.ConcurrentActionHandler
-import com.google.gson.JsonParser
 import net.openid.appauth.AppAuthConfiguration
 import net.openid.appauth.AuthorizationException
 import net.openid.appauth.AuthorizationRequest
@@ -27,15 +26,7 @@ import net.openid.appauth.browser.BrowserAllowList
 import net.openid.appauth.browser.BrowserMatcher
 import net.openid.appauth.browser.VersionedBrowserMatcher
 import net.openid.appauth.connectivity.DefaultConnectionBuilder
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
-import okhttp3.Response
-import java.io.IOException
 import java.util.Locale
-import java.util.concurrent.TimeUnit
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -259,51 +250,8 @@ class AuthenticatorImpl(
      */
     override suspend fun getUserInfo(): OAuthUserInfo {
 
-        // First get the current access token
-        var accessToken = this.getAccessToken()
-
-        // Get metadata if required
-        this.getMetadata()
-
-        // Make the userinfo request
-        var response = this.makeUserInfoRequest(accessToken)
-        if (response.isSuccessful) {
-
-            // Handle successful responses
-            return this.deserializeUserInfo(response)
-        } else {
-
-            // Retry once with a new token if there is a 401 error
-            if (response.code == 401) {
-
-                // Try to refresh the access token
-                accessToken = this.refreshAccessToken()
-
-                // Retry the userinfo request
-                response = this.makeUserInfoRequest(accessToken)
-                if (response.isSuccessful) {
-
-                    return this.deserializeUserInfo(response)
-
-                } else {
-
-                    // Handle failed responses on the retry
-                    throw ErrorFactory().fromHttpResponseError(
-                        response,
-                        this.configuration.userInfoEndpoint,
-                        "authorization server"
-                    )
-                }
-            } else {
-
-                // Handle failed responses on the original call
-                throw ErrorFactory().fromHttpResponseError(
-                    response,
-                    this.configuration.userInfoEndpoint,
-                    "authorization server"
-                )
-            }
-        }
+        val client = UserInfoClient(this.configuration, this)
+        return client.getUserInfo()
     }
 
     /*
@@ -360,61 +308,6 @@ class AuthenticatorImpl(
             val authService = AuthorizationService(this.applicationContext)
             authService.performTokenRequest(tokenRequest, NoClientAuthentication.INSTANCE, callback)
         }
-    }
-
-    /*
-     * Make a user info request with the current access token
-     */
-    private suspend fun makeUserInfoRequest(accessToken: String): Response {
-
-        val body: RequestBody? = null
-        val builder = Request.Builder()
-            .header("Accept", "application/json")
-            .header("Authorization", "Bearer $accessToken")
-            .method("GET", body)
-            .url(this.configuration.userInfoEndpoint)
-        val request = builder.build()
-
-        val client = OkHttpClient.Builder()
-            .callTimeout(10, TimeUnit.SECONDS)
-            .build()
-
-        val that = this@AuthenticatorImpl
-        return suspendCoroutine { continuation ->
-
-            client.newCall(request).enqueue(object : Callback {
-                override fun onResponse(call: Call, response: Response) {
-
-                    continuation.resumeWith(Result.success(response))
-                }
-
-                override fun onFailure(call: Call, e: IOException) {
-
-                    val exception = ErrorFactory().fromHttpRequestError(
-                        e,
-                        that.configuration.userInfoEndpoint,
-                        "authorization server"
-                    )
-                    continuation.resumeWithException(exception)
-                }
-            })
-        }
-    }
-
-    /*
-     * Deserialize the response data for user info
-     */
-    private fun deserializeUserInfo(response: Response): OAuthUserInfo {
-
-        if (response.body == null) {
-            throw IllegalStateException("Unable to deserialize HTTP response into user info")
-        }
-
-        val tree = JsonParser.parseString(response.body?.string())
-        val data = tree.asJsonObject
-        val givenName = data.get("given_name")?.asString ?: ""
-        val familyName = data.get("family_name")?.asString ?: ""
-        return OAuthUserInfo(givenName, familyName)
     }
 
     /*
