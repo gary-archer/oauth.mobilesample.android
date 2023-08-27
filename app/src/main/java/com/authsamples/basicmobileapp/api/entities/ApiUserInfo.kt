ec787e90505a61498c29aa4cfafd5b17c54a6d68
Gary Archer <32388530+gary-archer@users.noreply.github.com>
2023-08-27 20:28:02 +0100

    Update ApiUserInfo.kt

--- app/src/main/java/com/authsamples/basicmobileapp/api/entities/ApiUserInfo.kt
+++ app/src/main/java/com/authsamples/basicmobileapp/api/entities/ApiUserInfo.kt
@@ -4,6 +4,6 @@ package com.authsamples.basicmobileapp.api.entities
  * User attributes from the API's own data
  */
 data class ApiUserInfo(
-    val role: String,
+    val title: String,
     val regions: ArrayList<String>
 )
