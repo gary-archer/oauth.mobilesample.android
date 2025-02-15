80d189285bcfc7012ab31f375ffc3eb5ee18fabd
gary-archer <garyarcher36@gmail.com>
2025-02-15 14:14:12 +0000

    Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     defaultConfig {
         applicationId = "com.authsamples.finalmobileapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
